794352f0eeae4f4594fb25363fbfa9a9d39b91de
Almoyano <a.moyano.miguel@gmail.com>
2021-06-16 00:24:48 +0200

    Add fetch and API

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import getApiData from "../services/api";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getApiData().then((charactersData) => {
+      setCharacters(charactersData);
+    });
+  }, []);
+
   return (
     <>
       <h1 className="title">Listado de personajes</h1>
---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const getApiData = () => {
+  return fetch(
+    "https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json?results=20"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const cleanData = data.results.map((character) => {
+        console.log(character.name);
+        return {
+          photo: character.image,
+          name: character.name,
+          specie: character.species,
+        };
+      });
+      return cleanData;
+    });
+};
+
+export default getApiData;
